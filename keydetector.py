6ffdf10c77fb481b32f8d89f48761c166dfaa534
multifeb13 <musicfair@gmail.com>
2022-04-30 21:32:06 +0900

    [feat] add argument
    '-i/--input'

--- keydetector.py
+++ keydetector.py
@@ -1,5 +1,14 @@
+import argparse
+
+def get_arg():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i','--input', required=True, type=argparse.FileType('rb'))
+	return parser.parse_args()
+
 def main():
 	return None
 
 if __name__ == '__main__':
-    main()
+	args = get_arg()
+	print(args.input)
+	main()
